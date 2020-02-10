9de68c676588f508b613bc4484c835ab957e44ff
BitTim <tim.anhalt.2004@protonmail.com>
2020-02-10 10:47:54 +0100

    Added template for ResourceManager for later development

---
+++ lib/resource_manager.hh
@@ -0,0 +1,13 @@
+#ifndef RESOURCE_MANAGER_HH
+#define RESOURCE_MANAGER_HH
+
+//Resource variables:
+
+//Resource Loader:
+int loadResources()
+{
+  //Load resources here
+  return 0;
+}
+
+#endif // RESOURCE_MANAGER_HH
